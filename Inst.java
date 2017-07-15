d2ebb573fc6373b39120d602d3f01a358b2fff53
Arshad <arshad.mohammed18@gmail.com>
2017-07-15 15:22:32 +0530

    Institute Class - Corrected

--- Inst.java
+++ Inst.java
@@ -12,10 +12,10 @@ public class Inst {
 
     public Long iID; 
     public String iName;
-    public String iLocation;
     public Long iContact1;
     public Long iContact2;
     public String iEmail;
+    public String iLocation;
     public Long iLat;
     public Long iLong;
     public String iDate;
@@ -26,13 +26,13 @@ public class Inst {
         // Default constructor required for calls to DataSnapshot.getValue(Post.class)
     }
 
-    public Post(Long iid, String iname, String ilocation, Long icontact1, Long icontact2,String iemail, Long ilat, Long ilong, String idate, String ipublished, Long idid) {
+    public Post(Long iid, String iname, Long icontact1, Long icontact2, String iemail, String ilocation, Long ilat, Long ilong, String idate, String ipublished, Long idid) {
         this.iID = iid;
         this.iName = iname;
-        this.iLocation = ilocation;
         this.iContact1 = icontact1;
         this.iContact2 = icontact2;
         this.iEmail = iemail;
+        this.iLocation = ilocation;
         this.iLat = ilat;
         this.iLong = ilong;
         this.iDate = idate;
@@ -44,10 +44,10 @@ public class Inst {
         HashMap<String, Object> result = new HashMap<>();
         result.put("iID", dID);
         result.put("iName", dName);
-        result.put("iLocation",iLocation);
         result.put("iContact1",iContact1);
         result.put("iContact2",iContact2);
         result.put("iEmail",iEmail);
+        result.put("iLocation",iLocation);
         result.put("iLat",iLat);
         result.put("iLong",iLong);
         result.put("iDate",iDate);
